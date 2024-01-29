ff4110b7d7d991634ec0958dcac6bf204df4f5d6
sscoderZ <sakshisinghss9358@gmail.com>
2024-01-29 22:05:50 +0530

    Added isAbove18 fn

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const age=24;
-const name = 'Sakshi Singh'
+const name1 = 'Sakshi Singh'
 const dob = '12/08/2003'
 
 function sayName() {
-  console.log('My Name is ' + name)
+  console.log('My Name is ' + name1)
+}
+
+function isAbove18() {
+  return age >=18
 }
